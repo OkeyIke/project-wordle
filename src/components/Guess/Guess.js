169819f27bec778730a3b292d299ee19d4784edc
OkeyIke <benjaminwerner16@gmail.com>
2024-03-31 19:17:34 +0200

    feat(Game, GuessResults, Guess): add game logic for styling each letter cell based on 3 possible statuses

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { range } from '../../utils'
+import { checkGuess } from '../../game-helpers'
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer)
+  console.log(result)
   return (
     <p className='guess'>
       {range(5).map((colIndex) => (
-        <span key={colIndex} className='cell'>
+        <span key={colIndex} className={`${result ? result[colIndex].status: undefined} cell`}>
           {value ? value[colIndex] : undefined}
         </span>
       ))}
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,11 @@ import Guess from '../Guess'
 import { range } from '../../utils'
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className='guess-results'>
       {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => (
-        <Guess key={rowIndex} value={guesses[rowIndex]} />
+        <Guess key={rowIndex} value={guesses[rowIndex]} answer={answer} />
       ))}
     </div>
   )
